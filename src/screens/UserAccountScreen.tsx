7275224a68bea6f2d11d7d742519ebeaf5cfc17a
Trần Trung Hiếu <trantrunghieu.mail@gmail.com>
2024-11-20 18:11:42 +0700

    Add initial screen components and theme definitions

---
+++ src/screens/UserAccountScreen.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const UserAccountScreen = () => {
+    return (
+        <View style = {styles.container}>
+        <Text>Home Screen</Text>
+        </View>
+    );
+    };
+    
+const styles = StyleSheet.create({
+    container: {},
+});
+
+export default UserAccountScreen;
