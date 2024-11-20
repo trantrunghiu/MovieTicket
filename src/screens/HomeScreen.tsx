05b671e1b45620d88119997ebd5f6c41d3496529
Trần Trung Hiếu <trantrunghieu.mail@gmail.com>
2024-11-21 00:01:19 +0700

    Add loading profile

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,18 +1,60 @@
-import * as React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import CustomIcon from '../components/CustomIcon';
+import React, {useEffect, useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Dimensions,
+  TouchableOpacity,
+  ActivityIndicator,
+  ScrollView,
+  StatusBar,
+  FlatList,
+} from 'react-native';
+import {COLORS, SPACING} from '../themes/theme';
+import {
+  upcomingMovies,
+  nowPlayingMovies,
+  popularMovies,
+  baseImagePath,
+} from '../api/apicalls';
+const {width, height} = Dimensions.get('window');
 const HomeScreen = () => {
-    return (
-        <View style = {styles.container}>
-        <Text>Home Screen</Text>
+const [upcomingMoviesList, setupcomingMoviesList] = useState<any>(undefined);
+const [nowPlayingMoviesList, setnowPlayingMoviesList] = useState<any>(undefined);
+const [popularMoviesList, setpopularMoviesList] = useState<any>(undefined);
+if (!upcomingMoviesList || !nowPlayingMoviesList || !popularMoviesList) {
+
+  return (   
+     <ScrollView
+        style={styles.container}
+        bounces={false}
+        contentContainerStyle={styles.scrollViewContainer}>
+        <StatusBar hidden />
+        <View>
         </View>
-       
-        
-    );
-    };
-    
+        <View style={styles.loadingContainer}>
+          <ActivityIndicator size={'large'} color={COLORS.Orange} />
+        </View>
+      </ScrollView>
+
+  );
+}
+return <View style={styles.container}></View>;
+
+}; 
 const styles = StyleSheet.create({
-    container: {},
+   container: {
+    display: 'flex',
+    backgroundColor: COLORS.Black,
+  },
+  scrollViewContainer: {
+    flex: 1,
+  },
+  loadingContainer: {
+    flex: 1,
+    alignSelf: 'center',
+    justifyContent: 'center',
+  }
 });
 
 export default HomeScreen;
